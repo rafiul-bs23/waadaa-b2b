18678fd0e7a9c1bd3f8747d781f269673e1f0cea
rafiul-bs23 <rafiul.islam@brainstation-23.com>
2025-05-26 15:32:59 +0600

    Feat: WDT-0000/Font family and tailwind setup

---
+++ tailwind.config.ts
@@ -0,0 +1,83 @@
+// import { containerPlugin } from '@/utils/tailwind-plugins';
+
+import type { Config } from "tailwindcss";
+
+const containerScreens = {
+  sm: "540px",
+  md: "720px",
+  lg: "960px",
+  xl: "1140px",
+  "2xl": "1320px",
+  "3xl": "1440px",
+};
+
+const screens = {
+  xs: "375px",
+  ...containerScreens,
+};
+
+const padding = {
+  DEFAULT: "1rem", // Default padding for screens smaller than `sm`
+  xs: "1rem",
+  sm: "1rem",
+  md: "2rem",
+  lg: "3rem",
+  xl: "4rem",
+  "2xl": "4.5rem",
+  "3xl": "5rem",
+};
+
+const fontSize = {
+  "xs-rem": "12.5px", // for tiny screens
+  "sm-rem": "12px", // for small screens (540px)
+  "md-rem": "13px", // for medium screens (720px)
+  "lg-rem": "14.5px", // for large screens (960px)
+  "xl-rem": "15px", // for extra large screens (1140px)
+  "2xl-rem": "16px", // for 2xl screens (1320px)
+  "3xl-rem": "16px", // for 3xl screens (1440px)
+};
+
+const fontFamily = {
+  "public-sans": ["var(--wd-font-public-sans)", "sans-serif"],
+  "dm-serif-display": ["var(--wd-font-dm-serif-display)", "sans-serif"],
+  inter: ["var(--wd-font-inter)", "sans-serif"],
+  montserrat: ["var(--wd-font-montserrat)", "sans-serif"],
+};
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "var(--wd-primary)",
+        secondary: "var(--wd-secondary)",
+        ternary: "var(--wd-sonic-silver)",
+        "wd-neutral": {
+          600: "var(--wd-naro)",
+        },
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        "active-background-color":
+          "var(--wd-buttons-primary-active-background)",
+      },
+      fontSize,
+      screens,
+      fontFamily,
+    },
+
+    container: {
+      center: true,
+      padding,
+      screens: containerScreens,
+    },
+  },
+  plugins: [
+    /*containerPlugin*/
+  ],
+};
+
+export default config;
