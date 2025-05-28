9c980a318c03053a36ec85fb4e6b87736c43b9df
rafiul-bs23 <rafiul.islam@brainstation-23.com>
2025-05-28 17:29:40 +0600

    Feat: WDT-0000/Add country-by-currency-name-with-phone file

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 
-// import { handleLanguageMiddleware } from '@/middlewares/handle-language-middleware';
+import { handleLanguageMiddleware } from "@/middlewares/handle-language-middleware";
 import { handleUrlMiddleware } from "@/middlewares/handle-url-middleware";
 
-// export const defaultRegion = 'bd';
+export const defaultRegion = "bd";
 export const defaultPath = "home";
 
 export function middleware(request: NextRequest) {
   let response = NextResponse.next();
 
-  // response = handleLanguageMiddleware(request, response);
+  response = handleLanguageMiddleware(request, response);
 
   response = handleUrlMiddleware(request, response);
 
---
+++ src/types/pages/sign-in.tsx
@@ -0,0 +1,13 @@
+export type CountryByCurrencyWithPhoneName = {
+  name: string;
+  name_official: string;
+  alpha_2: string;
+  alpha_3: string;
+  country_code: string;
+  currency_name: string;
+  currency_code: string;
+  currency_number: string;
+  country_code_iso: string;
+  ccTLD: string;
+  phone_code: string;
+};
